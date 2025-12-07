007b7df8df96c0357683c46154786fecbc777448
Айзиля <ayzilya_ipkaeva@mail.ru>
2025-12-07 20:37:06 +0300

    LB3

--- Desktop/MainWindow.xaml.cs
+++ Desktop/MainWindow.xaml.cs
@@ -12,27 +12,91 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static Desktop.Class;
 
 namespace Desktop
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        UserRepository UR = new UserRepository();
+        Class validate = new Class();
+
         public MainWindow()
         {
             InitializeComponent();
+            UR.UserRegistration("AYZILYA", "04052007", "ayzilya@gmail.com");
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void LogIn(object sender, RoutedEventArgs e)
         {
+            string email = TBEmail.Text.Trim().ToLower();
+            string password = TBPassword.Text.Trim();
 
+            //Проверка полей на правильность с последующий входом
+
+            if (!Class.ValidateEmail(email))
+            {
+                MessageBox.Show("некорректная почта.", "ошибка");
+                return;
+            }
+
+            if (!Class.ValidatePassword(password))
+            {
+                MessageBox.Show("пароль должен содержать шесть символов.", "ошибка");
+                return;
+            }
+            try
+            {
+                var user = UR.UserAuthenticate(email, password);
+                MainEmpty main_Empty = new MainEmpty();
+                main_Empty.Show();
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ошибка");
+                return;
+            }
         }
 
-        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        private void Registation(object sender, RoutedEventArgs e)
         {
-
+            Registration registration = new Registration();
+            registration.Show();
+            this.Close();
         }
+
+        private void TBEmail_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (TBEmail.Text == "почта")
+            {
+                TBEmail.Text = string.Empty;
+            }
+        }
+
+        private void TBEmail_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TBEmail.Text))
+            {
+                TBEmail.Text = "Почта";
+            }
+        }
+        private void TBPassword_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (TBPassword.Text == "Пароль")
+            {
+                TBPassword.Text = string.Empty;
+            }
+        }
+
+        private void TBPassword_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TBPassword.Text))
+            {
+                TBPassword.Text = "Пароль";
+            }
+        }
+
     }
 }
+
--- Desktop/Registration.xaml.cs
+++ Desktop/Registration.xaml.cs
@@ -19,9 +19,98 @@ namespace Desktop
     /// </summary>
     public partial class Registration : Window
     {
+        UserRepository UR = new UserRepository();
+        Class Validate = new Class();
         public Registration()
         {
             InitializeComponent();
         }
+
+        private void Имя_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Имя.Text == "введите имя пользователя") { Имя.Foreground = this.Foreground; }
+            else if (Имя.Text == "имя не должно быть меньше трех символов") { Имя.Foreground = Brushes.Red; }
+            else Имя.Foreground = Brushes.Black;
+        }
+        private void EmailValidation(object sender, RoutedEventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            if (!Class.ValidateEmail(txt.Text))
+            {
+                Class.ShowError(txt, "неверный формат почты");
+            }
+        }
+
+        private void PasswordValidation(object sender, RoutedEventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            if (!Class.ValidatePassword(txt.Text))
+            {
+                Class.ShowError(txt, "пароль не должен быть меньше шести символов");
+            }
+        }
+
+        private void NameValidation(object sender, RoutedEventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            if (!Class.ValidateName(txt.Text))
+            {
+                Class.ShowError(txt, "имя не должно быть меньше трех символов");
+            }
+        }
+
+        private void BackToLogIn(object sender, RoutedEventArgs e)
+        {
+            var LogIn = new MainWindow();
+            LogIn.Show();
+            this.Close();
+
+        }
+
+        private void Registation(object sender, RoutedEventArgs e)
+        {
+            string email = TBEmail.Text.Trim().ToLower();
+            string password = Пароль.Text.Trim();
+            string repeatPassword = Повтор.Text.Trim();
+            string login = Имя.Text.Trim();
+
+            if (Имя.Text == "имя не должно быть меньше трех символов" || Имя.Text == "введите имя пользователя") { MessageBox.Show("неверно введены данные"); }
+            else if (Пароль.Text == "пароль не должен быть меньше шести символов" || Пароль.Text == "введите пароль") { MessageBox.Show("неверно введены данные"); }
+            else if (TBEmail.Text == "неверный формат почты" || TBEmail.Text == "ayz@yandex.ru") { MessageBox.Show("неверно введены данные"); }
+            else if (Повтор.Text == "повторите пароль") { MessageBox.Show("неверно введены данные"); }
+            else if (UR.UserRegistration(login, password, email))
+            {
+                MainEmpty main_Empty = new MainEmpty();
+                main_Empty.Show();
+                this.Close();
+            }
+            else
+            {
+                return;
+            }
+
+        }
+
+        private void email_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (TBEmail.Text == "ayz@yandex.ru") { TBEmail.Foreground = this.Foreground; }
+            else if (TBEmail.Text == "неверный формат почты") { TBEmail.Foreground = Brushes.Red; }
+            else TBEmail.Foreground = Brushes.Black;
+        }
+
+        private void Пароль_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Пароль.Text == "введите пароль") { Пароль.Foreground = this.Foreground; }
+            else if (Пароль.Text == "пароль не должен быть меньше шести символов") { Пароль.Foreground = Brushes.Red; }
+            else Пароль.Foreground = Brushes.Black;
+        }
+
+        private void Повтор_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Повтор.Text != Пароль.Text) { }
+            if (Повтор.Text == "повторите пароль") { Повтор.Foreground = this.Foreground; }
+            else Повтор.Foreground = Brushes.Black;
+        }
     }
 }
+
